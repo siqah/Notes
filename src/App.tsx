f0be2ea040364064bdfb37b524461592dcadb708
Siqah <faruoqmuhammed@zetech.ac.ke>
2024-03-02 23:12:38 +0300

    Intial

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { NewNote } from "./NewNote";
+import { Navigate, Route, Routes } from "react-router";
+
+function App() {
+
+  return (
+    <Container className="my-4"> 
+          <Routes>
+              <Route  path="/"    element={<h1>Home</h1>} />
+              <Route  path="/new" element={<NewNote />} />
+              <Route path="/:id">
+                <Route index element={<h1>Show</h1>} />
+                <Route path="edit" element={<h1>Edit</h1>} />
+              </Route>
+              <Route  path="*"  element={<Navigate to="/" />} />
+          </Routes> 
+   </Container>
+  )
+}
+
+export default App
